2a65ab46d61b15fe461b19d203e4d1a65c9062f3
Anton Konyahin <me@konyahin.xyz>
2025-05-20 22:30:55 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"io/fs"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"konyahin.xyz/passeta/view"
+)
+
+var passwords []string
+
+func main() {
+	passDir := os.Getenv("PASSWORD_STORE_DIR")
+	if passDir == "" {
+		log.Fatal("Can't find pass directory! $PASSWORD_STORE_DIR is empty.")
+	}
+
+	filepath.WalkDir(passDir, func(path string, d fs.DirEntry, err error) error {
+		switch {
+		case path == passDir:
+			return nil
+		case d.IsDir() && strings.Contains(path, "/."):
+			return fs.SkipDir
+		case d.IsDir() || strings.Contains(path, "/."):
+			return nil
+		}
+
+		name, _ := strings.CutPrefix(path, passDir)
+		name, _ = strings.CutSuffix(name, ".gpg")
+		passwords = append(passwords, name)
+		return nil
+	})
+
+	view.SetItems(passwords)
+
+	view.SetOnSearchCallback(func(search string) {
+		searchs := strings.Split(search, " ")
+		items := make([]string, 0, len(passwords))
+	pass:
+		for _, password := range passwords {
+			for _, searchPart := range searchs {
+				if !strings.Contains(password, searchPart) {
+					continue pass
+				}
+			}
+			items = append(items, password)
+		}
+		view.SetItems(items)
+	})
+
+	view.SetOnDoneCallback(func(name string, new bool) {
+		if new {
+			passwordChannel := make(chan string)
+			view.RequestPassword(passwordChannel)
+			go func() {
+				password := <-passwordChannel
+
+				cmd := exec.Command("pass", "insert", "-e", name)
+				cmd.Stdin = strings.NewReader(password)
+				output, err := cmd.CombinedOutput()
+				if err != nil {
+					view.SetStatusErrorString("error: " + string(output))
+				} else {
+					view.SetStatusString(name + " created")
+				}
+				view.Redraw()
+			}()
+			return
+		}
+
+		cmd := exec.Command("pass", "-c", name)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			view.SetStatusErrorString("error: " + string(output))
+		} else {
+			view.SetStatusString(name + " in your clipboard")
+		}
+	})
+
+	view.Run()
+}
---
+++ view/view.go
@@ -0,0 +1,116 @@
+package view
+
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+var (
+	app  *tview.Application = tview.NewApplication()
+	flex *tview.Flex        = tview.NewFlex()
+
+	passwordsList *tview.List       = tview.NewList()
+	searchField   *tview.InputField = tview.NewInputField()
+	statusText    *tview.TextView   = tview.NewTextView()
+
+	onSearch func(string)
+	onDone   func(string, bool)
+)
+
+func init() {
+	flex.SetDirection(tview.FlexRow).
+		AddItem(searchField, 1, 1, true).
+		AddItem(passwordsList, 0, 1, false).
+		AddItem(statusText, 1, 1, false)
+
+	viewInit()
+}
+
+func Run() {
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		log.Fatalf("Can't start user interface: %v", err)
+	}
+}
+
+func SetItems(items []string) {
+	passwordsList.Clear()
+	for _, item := range items {
+		passwordsList.AddItem(item, item, 0, nil)
+	}
+}
+
+func SetOnSearchCallback(callback func(string)) {
+	onSearch = callback
+}
+
+func SetOnDoneCallback(callback func(string, bool)) {
+	onDone = callback
+}
+
+func SetStatusString(status string) {
+	statusText.SetBackgroundColor(tcell.Color104)
+	statusText.SetText(status)
+}
+
+func SetStatusErrorString(status string) {
+	statusText.SetBackgroundColor(tcell.ColorDarkRed)
+	statusText.SetText(status)
+}
+
+func RequestPassword(passwordChan chan string) {
+	searchField.SetLabel("enter password:")
+	searchField.SetText("")
+	searchField.SetBackgroundColor(tcell.ColorDarkGreen)
+	searchField.SetMaskCharacter('*')
+	searchField.SetChangedFunc(nil)
+	searchField.SetDoneFunc(func(key tcell.Key) {
+		passwordChan <- searchField.GetText()
+		close(passwordChan)
+		viewInit()
+	})
+}
+
+func Redraw() {
+	app.Draw()
+}
+
+func viewInit() {
+	passwordsList.ShowSecondaryText(false)
+
+	searchField.SetLabel(":").
+		SetText("").
+		SetMaskCharacter(0).
+		SetDoneFunc(search).
+		SetChangedFunc(func(text string) {
+			onSearch(text)
+		})
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyCtrlN:
+			fallthrough
+		case tcell.KeyDown:
+			passwordsList.SetCurrentItem(passwordsList.GetCurrentItem() + 1)
+
+		case tcell.KeyCtrlP:
+			fallthrough
+		case tcell.KeyUp:
+			passwordsList.SetCurrentItem(passwordsList.GetCurrentItem() - 1)
+
+		case tcell.KeyCtrlQ:
+			app.Stop()
+		}
+		return event
+	})
+}
+
+func search(key tcell.Key) {
+	if index := passwordsList.GetCurrentItem(); index == passwordsList.GetItemCount() {
+		onDone(searchField.GetText(), true)
+	} else {
+		_, secondaryText := passwordsList.GetItemText(passwordsList.GetCurrentItem())
+		onDone(secondaryText, false)
+	}
+}
